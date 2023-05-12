2649fbf3c2666fae196e54cda4d75b2457187a43
Salah El-din Saeed Abu Saif <80893300+salahsaeed19@users.noreply.github.com>
2023-05-12 14:36:27 +0300

    version 5

--- src/shapes_singleton/Main.java
+++ src/shapes_singleton/Main.java
@@ -10,10 +10,10 @@ public class Main {
         Shape rectangle = factory.createShape("rectangle", 5, 10);
         Shape triangle = factory.createShape("triangle", 3, 4);
 
-        System.out.println("Circle area: " + circle.calculateArea()); // should print 78.5398...
-        System.out.println("Square area: " + square.calculateArea()); // should print 100.0...
-        System.out.println("Rectangle area: " + rectangle.calculateArea()); // should print 50.0...
-        System.out.println("Triangle area: " + triangle.calculateArea()); // should print 6.0...
+        System.out.println("Circle area: " + circle.calculateArea());
+        System.out.println("Square area: " + square.calculateArea());
+        System.out.println("Rectangle area: " + rectangle.calculateArea());
+        System.out.println("Triangle area: " + triangle.calculateArea());
         
     }
     
